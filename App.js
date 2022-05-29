ba63a1010cd2318cccd3311bb67d6e1c2b32d321
MonMaramba <monmaramba@yahoo.com>
2022-05-28 17:34:23 -0700

    react-native-paper added. Searchbar added.

--- App.js
+++ App.js
@@ -7,25 +7,28 @@ import {
   SafeAreaView,
   Platform,
 } from 'react-native';
+import { Searchbar } from 'react-native-paper';
 
 const isAndroid = Platform.OS === 'android';
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
-        <View style={{ padding: 16, backgroundColor: 'green' }}>
-          <Text>search</Text>
+      <SafeAreaView style={styles.safeAreaView}>
+        <View style={styles.search}>
+          <Searchbar />
         </View>
-        <View style={{ flex: 1, padding: 16, backgroundColor: 'blue' }}>
+        <View style={styles.list}>
           <Text>list</Text>
         </View>
       </SafeAreaView>
-      <ExpoStatusBar style={{ backgroundColor: 'orange' }} />
+      <ExpoStatusBar  />
     </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  safeAreaView: { flex: 1, marginTop: StatusBar.currentHeight },
+  search: { padding: 16, backgroundColor: 'green' },
+  list: { flex: 1, padding: 16, backgroundColor: 'blue' }
 });
