f9f313d65047cafbdd501925ae64b14a6a750a43
MonMaramba <monmaramba@yahoo.com>
2022-06-01 20:49:46 -0700

    added isOpen icon

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,19 +1,33 @@
 import React from 'react';
-import { Text, StyleSheet, View } from 'react-native';
+import { Text, StyleSheet, View, Image } from 'react-native';
 import styled from 'styled-components/native';
 import { SvgXml } from 'react-native-svg';
 import { Card } from 'react-native-paper';
 import star from '../../../../assets/star';
+import open from '../../../../assets/open';
 
 const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.ui.primary};
+  padding: ${(props) => props.theme.space[3]};
 `;
 
 const Rating = styled(View)`
   flex-direction: row;
-  background-color: ${(props) => props.theme.colors.bg.primary};
+
   padding-top: ${(props) => props.theme.space[2]};
   padding-bottom: ${(props) => props.theme.space[2]};
+`;
+
+const Section = styled(View)`
+  flex-direction: row;
+  align-items: center;
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+const SectionEnd = styled(View)`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
 `;
 
 const Info = styled(View)`
@@ -56,11 +70,16 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
       <Info>
         <Title>{name}</Title>
-        <Rating>
-          {ratingArray.map(() => (
-            <SvgXml xml={star} width={20} height={20} />
-          ))}
-        </Rating>
+        <Section>
+          <Rating>
+            {ratingArray.map(() => (
+              <SvgXml xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+          <SectionEnd>
+            {isOpenNow ? <SvgXml xml={open} width={20} height={20} /> : null}
+          </SectionEnd>
+        </Section>
         <Address>{address}</Address>
       </Info>
     </RestaurantCard>
